cbe4d83fbd57ed2b2fc8fb1071267e81a2c4295c
PedroB <pedrob@craisasede.corp>
2022-01-05 11:29:28 -0300

    todo-list

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputElement = document.getElementById("input");
 let tarefas = JSON.parse(localStorage.getItem("salvar")) || [];
 
     function addTarefa() {
+        
 
         var inputValue = inputElement.value;
 
@@ -26,26 +27,60 @@ let tarefas = JSON.parse(localStorage.getItem("salvar")) || [];
     function listTask() {
 
 
+        ulList.innerHTML = "";
+
+            for(const tarefa of tarefas) {
+                
+                console.log(tarefa);
+                const liList = document.createElement("li");
+                const textTask = document.createTextNode(tarefa);
+                const id = tarefas.indexOf(tarefa);
+
+                const apagarTask = document.createElement("ion-icon");
+                apagarTask.setAttribute("name","trash-outline");
+                // <ion-icon name="trash-outline"></ion-icon>
+
+                liList.appendChild(textTask);
+                ulList.appendChild(liList);
+                liList.appendChild(apagarTask);
+
+                apagarTask.setAttribute("onclick",`FapagarTask(${id})`);
+
+            }
 
 }
+
+listTask()
 
     function salvar() {
 
         localStorage.setItem("salvar", JSON.stringify(tarefas));
 
-
 }
 
     function teste() {
 
-    console.log(inputElement.value);
+        console.log(inputElement.value);
 
 }
+
+    function FapagarTask(id) {
+        
+            setTimeout(() => {
+                
+                tarefas.splice(id,1);
+                listTask();
+                salvar();
+
+                }, 1000);
+
+    }
 
     function limpar() {
         console.log("ta removendo ta?");
         tarefas = [];
         localStorage.clear();
+        ulList.innerHTML = "";
     }
 
     addEventListener('keypress',e=>{
